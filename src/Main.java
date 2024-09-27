d42ea3c86441eedc1c89bab970353d64d0c0d164
Corey Mays <corey.mays17@gmail.com>
2024-09-26 22:21:46 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,16 @@ public class Main {
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
+        int age = 0;
+        final int WRISTBAND_AGE = 21;
+        System.out.print("Enter your age: ");
+        if (in.hasNextInt())
+        {
+            age = in.nextInt();
+            if (age >= WRISTBAND_AGE)
+            {
+                System.out.println("You're " + age + " you get a wristband.");
+            }
+        }
     }
 }
